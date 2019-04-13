c20322137c411863c7ddd4431225ddebd0d48ccf
zhangtaoya <zhangtaoya@aliyun.com>
2019-04-13 17:42:10 +0800

    MOD:xx

--- robot/manager.py
+++ robot/manager.py
@@ -11,7 +11,7 @@ from service import wechat_service
 
 from worker import robot_processor
 while True:
-    time.sleep(1)
+    time.sleep(1.5)
     col_account = db.get_col_wechat_account_sync()
     doc = mongo.mongo_find_one(col_account, {'status': wechat_service.WECHAT_ACCOUNT_STATUS_INIT})
     if doc is False:

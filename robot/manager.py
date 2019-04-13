d328b18b9325a0d5d707bffdd8bff47b2c30fe0f
zhangtaoya <zhangtaoya@aliyun.com>
2019-04-13 17:41:27 +0800

    MOD:xx

--- robot/manager.py
+++ robot/manager.py
@@ -11,7 +11,7 @@ from service import wechat_service
 
 from worker import robot_processor
 while True:
-    time.sleep(0.5)
+    time.sleep(1)
     col_account = db.get_col_wechat_account_sync()
     doc = mongo.mongo_find_one(col_account, {'status': wechat_service.WECHAT_ACCOUNT_STATUS_INIT})
     if doc is False:
@@ -20,7 +20,7 @@ while True:
         continue
 
     if not doc:
-        log.error("no wechat robot come in")
+        log.info("no wechat robot come in")
         continue
     mongo.mongo_update_one(col_account,
                            {'_id': doc['_id']},
